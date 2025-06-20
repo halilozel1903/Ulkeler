88851e4687630ddc58f2c246b2c4e3a29a4ba7db
Halil Ozel <halilozel1903@gmail.com>
2025-06-20 23:14:47 +0300

    feat(model): Create Ulke data class with Parcelable support

--- app/src/main/java/com/halil/ozel/ulkeler/Ulke.kt
+++ app/src/main/java/com/halil/ozel/ulkeler/Ulke.kt
@@ -1,3 +1,13 @@
 package com.halil.ozel.ulkeler
 
-data class Ulke(var isim: String, var resim: Int)
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Ulke(
+    val adi: String,
+    val baskent: String,
+    val yuzolcumu: Double,
+    val dil: String,
+    val bayrakKodu: String
+) : Parcelable
