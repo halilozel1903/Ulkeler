7b4f8e10d9288658fb884600ca90e00174c4f159
Halil Ozel <halilozel1903@gmail.com>
2025-06-21 00:16:44 +0300

    fix(search): use full TextWatcher implementation for EditText filtering

--- app/src/main/java/com/halil/ozel/ulkeler/MainActivity.kt
+++ app/src/main/java/com/halil/ozel/ulkeler/MainActivity.kt
@@ -2,6 +2,8 @@ package com.halil.ozel.ulkeler
 
 import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.halil.ozel.ulkeler.databinding.ActivityMainBinding
@@ -13,22 +15,31 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // JSON'dan ülke listesini oku
+        // JSON'dan veri oku
         val ulkeListesi = AssetHelper.okuUlkeleri(this)
 
-        // Adapter'ı oluştur - tıklanınca DetayActivity'ye git
         adapter = UlkeAdapter(ulkeListesi) { secilenUlke ->
             val intent = Intent(this, DetayActivity::class.java)
             intent.putExtra("ulke", secilenUlke)
             startActivity(intent)
         }
 
-        // RecyclerView ayarları
         binding.recyclerView.layoutManager = LinearLayoutManager(this)
         binding.recyclerView.adapter = adapter
+
+        binding.editTextSearch.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                adapter.filter.filter(s?.toString())
+            }
+
+            override fun afterTextChanged(s: Editable?) {}
+        })
+
+
     }
 }
